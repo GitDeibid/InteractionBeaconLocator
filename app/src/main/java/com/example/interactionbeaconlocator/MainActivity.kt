4e1fc60f9902167a08144b96e1d87f926b70dbed
david <darm1452@gmail.com>
2022-05-13 00:24:33 -0400

    Configuraciones varias

--- app/src/main/java/com/example/interactionbeaconlocator/MainActivity.kt
+++ app/src/main/java/com/example/interactionbeaconlocator/MainActivity.kt
@@ -8,6 +8,7 @@ import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
@@ -26,38 +27,34 @@ class MainActivity : AppCompatActivity() {
     private val adaptador:BluetoothAdapter by lazy {
         (getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter
     }
-
     private var bluetoothLeScanner:BluetoothLeScanner?= null
+    private val scanConfig = ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_POWER).build()
     //private val escaner = adaptador.bluetoothLeScanner
     private var scanning = false
     private val handler = Handler()
-    private val SCAN_PERIOD:Long = 10000
-    var ROL:String = "Anónimo"
+    var SCAN_PERIOD:Long = 60000
+    var time_minutos:Int?=null
+    var ROL:String? = "Anónimo"
+    var rssi:String?=null
+    var adress:String?=null
+    var nombre:String?=null
+
     private val leScanCallback = object : ScanCallback() {
 
         @SuppressLint("MissingPermission")
         override fun onScanResult(callbackType: Int, result: ScanResult?) {
             super.onScanResult(callbackType, result)
 
-            var rssi=result?.rssi.toString()
-            var adress = result?.device?.address.toString()
-            var nombre = result?.device?.name.toString()
-            if (adress=="60:77:71:8E:69:B9" || adress == "60:77:71:8E:72:85"){
-                crud?.newRegistro(Registro(nombre,rssi,adress,ROL))
-                Log.d("ESCANER","onScanResult: ${result?.device?.name}")
+            rssi=result?.rssi.toString()
+            adress = result?.device?.address.toString()
+            nombre = result?.device?.name.toString()
+            //if (adress=="60:77:71:8E:69:B9" || adress == "60:77:71:8E:72:85")
+            if (result?.device!=null){
+                crud?.newRegistro(Registro(nombre!!,rssi!!,adress!!,ROL!!))
+                Log.d("ESCANER","Dispositivo: ${result?.device?.name}")
             }
-        }
 
-        override fun onBatchScanResults(results: MutableList<ScanResult>?) {
-            super.onBatchScanResults(results)
-            Log.d("ESCANER","onBatchScanResults:${results.toString()}")
         }
-
-        override fun onScanFailed(errorCode: Int) {
-            super.onScanFailed(errorCode)
-            Log.d("ESCANER", "onScanFailed: $errorCode")
-        }
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,14 +74,23 @@ class MainActivity : AppCompatActivity() {
         val rol_adapter = ArrayAdapter(this,R.layout.rols_items,roles)
         binding.DDRoles.setAdapter(rol_adapter)
 
-
+        //Drop down menú de periodos de tiempo de escaner.
+        val escaner_time = resources.getStringArray(R.array.scanner_times)
+        val escaner_time_adapter = ArrayAdapter(this,R.layout.time_list,escaner_time)
+        binding.DDTime.setAdapter(escaner_time_adapter)
+        //Barra de progreso de avance de escanner.
+        var barraCarga = binding.progressBar
+        barraCarga.max=9000
+        //Tiempo de escanner.
+        var tiempo = binding.DDTime.text.toString()
+        //Boton de escanear.
         binding.btnEscanear.setOnClickListener{
-            //Barra de progreso de avance de escanner.
-            binding.progressBar.max=10000
-            //val currentProgress = 6000
-            ObjectAnimator.ofInt(binding.progressBar,"progress",10000).setDuration(SCAN_PERIOD).start()
-
             if (adaptador.isEnabled){
+                if(tiempo!=""){
+                    SCAN_PERIOD = tiempo.toLong()*60*1000
+                }
+                //val currentProgress = 6000
+                ObjectAnimator.ofInt(barraCarga,"progress",9000).setDuration(SCAN_PERIOD).start()
                 ROL = binding.DDRoles.text.toString()
                 //Se debe escanear en bsuca de los dispositivos.
                 escanearBLE()
@@ -107,6 +113,7 @@ class MainActivity : AppCompatActivity() {
             ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED ||
             ContextCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH)!=PackageManager.PERMISSION_GRANTED ||
             ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED ||
             ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED ||
             ContextCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH_SCAN)!= PackageManager.PERMISSION_GRANTED
                 ){
@@ -118,19 +125,20 @@ class MainActivity : AppCompatActivity() {
     private fun solicitarPermiso() {
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.BLUETOOTH,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.BLUETOOTH_CONNECT,
-            Manifest.permission.BLUETOOTH_SCAN
+            Manifest.permission.BLUETOOTH_SCAN,Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE
             ),777)
     }
 
+    @SuppressLint("MissingPermission")
     private fun escanearBLE(){
-        if (ActivityCompat.checkSelfPermission(
+        /*if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.BLUETOOTH_SCAN
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             ActivityCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH_SCAN)
             Log.d("ESCANER","Permiso de scanner no entregado...")
-        }
+        }*/
         if (!scanning) { // Stops scanning after a pre-defined scan period.
             handler.postDelayed({
                 scanning = false
@@ -138,7 +146,7 @@ class MainActivity : AppCompatActivity() {
                 Log.d("ESCANER","Deteniendo el escaneo")
             }, SCAN_PERIOD)
             scanning = true
-            bluetoothLeScanner!!.startScan(leScanCallback)
+            bluetoothLeScanner!!.startScan(null,scanConfig,leScanCallback)
             Log.d("ESCANER","Se inicia el escaneo")
         } else {
             scanning = false
